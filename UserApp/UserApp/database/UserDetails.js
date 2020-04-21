34e048d40dc9f4087d3f85cc391ffc6937ddeabb
NageswarV <37012134+NageswarV@users.noreply.github.com>
2020-04-21 13:21:22 +0530

    Add files via upload

---
+++ UserApp/UserApp/database/UserDetails.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    username: "userOne",
+    email: "userone@epam.com",
+    address: "hyderabad",
+  },
+  {
+    username: "usertwo",
+    email: "usertwo@epam.com",
+    address: "hyderabad",
+  },
+];
